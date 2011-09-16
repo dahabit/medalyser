f0e5053113f541131306dc7462daf11cb5394696
Mehdi Fanai <mehdifanai@gmail.com>
2011-09-16 17:37:57 +0000

    #patient store grid with fields columns and stores.

--- public/app/view/patient/ViewAll.js
+++ public/app/view/patient/ViewAll.js
@@ -16,27 +16,174 @@
  * @license GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link http://www.MedAlyser.com
  */
-Ext.define('MA.view.patient.ViewAll', {
-	extend : 'Ext.grid.Panel',
-	alias : 'widget.ViewAllPatients',
-	id:'ViewAllPatients',
-	//requires : [],
-	title : 'All Patients',
-	initComponent : function() {
-		 this.store = {
-		            fields: ['name', 'email'],
-		            data  : [
-		                {name: 'Ed',    email: 'ed@sencha.com'},
-		                {name: 'Tommy', email: 'tommy@sencha.com'}
-		            ]
-		        };
+Ext
+		.define(
+				'MA.view.patient.ViewAll',
+				{
+					extend : 'Ext.grid.Panel',
+					alias : 'widget.ViewAllPatients',
+					id : 'ViewAllPatients',
+					// requires : [],
+					title : 'All Patients',
+					dockedItems : [ {
+						xtype : 'toolbar',
+						dock : 'top',
+						items : [ {
+							text : 'Docked to the top'
+						} ]
+					} ],
+					stateId : 'state.ViewAllPatients',
+					initComponent : function() {//new MA.store.Patients();
+						patientsStore = Ext.create('MA.store.Patients');
+						//patientsStore=Ext.data.StoreManager.lookup('Patients');
+					this.store = patientsStore;
+						Ext.apply(this, {
+						    store: 'Patients'
+							}),
 
-		        this.columns = [
-		            {header: 'Name',  dataIndex: 'name',  flex: 1},
-		            {header: 'Email', dataIndex: 'email', flex: 1}
-		        ];
+						this.columns = [
+								{
+									header : 'Patient Id',
+									dataIndex : 'a_userid',
+									flex : .5
+								}/*
+									 * , { header : 'Photo', dataIndex :
+									 * 'a_profilephoto', flex : 1 }
+									 */,
+								{
+									header : 'Name',
 
-		        this.callParent(arguments);
+									columns : [ {
+										header : 'First',
+										dataIndex : 'a_firstname'
+									}, {
+										header : 'Middle',
+										dataIndex : 'a_middlename'
+									}, {
+										header : 'Last',
+										dataIndex : 'a_lastname'
+									} ]
+								},
+								{
+									header : 'Birth Date & Time',
+									dataIndex : 'a_birthdate',
+									renderer : function(value) {
+										if (value) {
+											dt = Ext.Date.parse(value.date,
+													"Y-m-d h:i:s");
+											return Ext.Date.format(dt,
+													'Y-m-d H:i:s')
+										}
+									},
+									// renderer
+									// :Ext.util.Format.dateRenderer('m/d/Y'),
+									flex : 1
+								},
+								{
+									header : 'Registeration Date & Time',
+									dataIndex : 'a_created',
+									renderer : function(value) {
+										if (value) {
+											dt = Ext.Date.parse(value.date,
+													"Y-m-d h:i:s");
+											return Ext.Date.format(dt,
+													'Y-m-d H:i:s')
+										}
+									},
+									flex : 1
+								},
+								{
+									header : 'SS#',
+									dataIndex : 'a_socialsecurity',
+									flex : 1
+								},
+								{
+									header : 'Sex',
+									dataIndex : 'a_sex',
+									flex : 1
+								},
+								{
+									header : 'Marital status',
+									dataIndex : 'a_maritalstatus',
+									hidden : true,
+									flex : 1
+								},
+								{
+									// TODO: remove preferred contact method and
+									// highlight preferred
+									// contact phone number automatically
+									header : 'Phone',
+									columns : [
+												{
+										header : 'Email',
+										dataIndex : 'a_primaryemail',
+										renderer : function(value,
+												metaData, record) {
+											var prefcontactmethod = record
+													.get('a_prefcontactmethod');
+											if (prefcontactmethod == 4) {
+												return '<div style="background-color:#f2e8da">'
+														+ value
+														+ '</div>';
+											}else {return value;}
+										}
+									},
+											{
+												header : 'Home',
+												dataIndex : 'a_homephone',
+												renderer : function(value,
+														metaData, record) {
+													var prefcontactmethod = record
+															.get('a_prefcontactmethod');
+													if (prefcontactmethod == 0) {
+														return '<div style="background-color:#f2e8da">'
+																+ value
+																+ '</div>';
+													}else {return value;}
+												}
+											}, {
+												header : 'Business',
+												dataIndex : 'a_businessphone',
+												renderer : function(value,
+														metaData, record) {
+													var prefcontactmethod = record
+															.get('a_prefcontactmethod');
+													if (prefcontactmethod == 1) {
+														return '<div style="background-color:#f2e8da">'
+																+ value
+																+ '</div>';
+													}else {return value;}
+												}
+											}, {
+												header : 'Mobile',
+												dataIndex : 'a_mobilephone',
+												renderer : function(value,
+														metaData, record) {
+													var prefcontactmethod = record
+															.get('a_prefcontactmethod');
+													if (prefcontactmethod == 2) {
+														return '<div style="background-color:#f2e8da">'
+																+ value
+																+ '</div>';
+													}else {return value;}
+												}
+											}, {
+												header : 'Fax',
+												dataIndex : 'a_faxphone',
+												renderer : function(value,
+														metaData, record) {
+													var prefcontactmethod = record
+															.get('a_prefcontactmethod');
+													if (prefcontactmethod == 3) {
+														return '<div style="background-color:#f2e8da">'
+																+ value
+																+ '</div>';
+													}else {return value;}
+												}
+											} ]
+								} ];
 
-	}
-});
+						this.callParent(arguments);
+
+					}
+				});
